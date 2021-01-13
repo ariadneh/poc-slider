cf6ba64da7ccc6aa6d5e62cf0af1b4f353e3f887
Weslley Cavalcante <whenriquerc@gmail.com>
2021-01-13 19:12:54 -0300

    second-version

--- src/App.js
+++ src/App.js
@@ -2,35 +2,40 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [minRange, setMinRange] = useState(0)
-  const [maxRange, setMaxRange] = useState(5000)
-  const handleMinRange = (e) => {
-    setMinRange(e.target.value)
-  }
-  const handleMaxRange = (e) => {
-    setMaxRange(e.target.value)
-  }
-  const showValues = () => {
-    if (maxRange < minRange) {
-      return (
-        <>
-          R$ {maxRange}
-          R$ {minRange}
-        </>
-      )
-    }
+  // const [minRange, setMinRange] = useState(0)
+  // const [maxRange, setMaxRange] = useState(5000)
 
+  const [range, setRange] = useState([0, 5000])
+  const [iMin, iMax] = [0, 5000]
+  const [min, max] = range
+
+  const handleRange = (e) => {
+    const { value } = e.target
+    const tAndrera = (value - iMin > iMax - value)
+
+    if ((value >= max) || (value >= min && value <= max && !tAndrera)) {
+      setRange([min, value])
+    } else if ((value <= min) || (value >= min && value <= max && tAndrera)) {
+      setRange([value, max])
+    }
+  }
+
+  const showValues = () => {
     return (
       <>
-        R$ {minRange}
-        R$ {maxRange}
+        <label htmlFor='minrange'>
+          R$ {min}
+        </label>
+        <label htmlFor='maxrange'>
+          R$ {max}
+        </label>
       </>
     )
   }
   return (
     <>
-      <input type='range' name='slider' id='slider' min='100' max='5000' step='100' onChange={handleMinRange} value={minRange} />
-      <input type='range' name='slider' id='slider' min='100' max='5000' value={maxRange} step='100' onChange={handleMaxRange} />
+      <input type='range' name='minrange' id='slider' min={iMin} max={iMax} defaultValue={iMin} step='100' onChange={handleRange} />
+      <input type='range' name='maxrange' id='slider-max' min={iMin} max={iMax} defaultValue={iMax} step='100' onChange={handleRange} />
       {showValues()}
       <span style={{ width: '100%', position: 'absolute' }} />
     </>
